8e74207a3546551866352ff1192575fe13a5957e
DEV-PROG-spec <60777466+DEV-PROG-spec@users.noreply.github.com>
2020-08-22 01:04:38 -0700

    Create LabTask2.java

---
+++ LabTask2.java
@@ -0,0 +1,29 @@
+//author:Devendra Srivastava..
+//sec:B
+package labWork;
+
+//create a program to declare,initialize and use local variable of all primitive datatype in java..
+public class Task2 {
+
+	public static void main(String[] args) {
+		byte byteVar = 112;
+		short shortVar = 25000;
+		char charVar = 'D';
+		int intVar = 125456;
+		long longVar = 12345678;
+		float floatVar = 5.26f;
+		double doubleVar = 5.2345;
+		boolean booleanVar = true;
+		System.out.println("Byte Datatype = " + byteVar);
+		System.out.println("Short Datatype = " + shortVar);
+		System.out.println("Char Datatype = " + charVar);
+		System.out.println("Int Datatype = " + intVar);
+		System.out.println("Long Datatype = " + longVar);
+		System.out.println("Float Datatype = " + floatVar);
+		System.out.println("Double Datatype = " + doubleVar);
+		System.out.println("Boolean Datatype = " + booleanVar);
+
+
+	}
+
+}
