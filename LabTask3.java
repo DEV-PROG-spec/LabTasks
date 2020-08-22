7b1f1c1a57973b1d985eff04226889f577a33ce9
DEV-PROG-spec <60777466+DEV-PROG-spec@users.noreply.github.com>
2020-08-22 01:11:58 -0700

    Create LabTask3.java

---
+++ LabTask3.java
@@ -0,0 +1,12 @@
+//Author:Devendra Srivastava(DEV-PROG-Spec)
+//Sec:B
+package labWork;
+
+//LabTask3:Create a program in Java to demonstrate declaration, initialization and usage of local variables
+public class LabTask3 {
+	public static void main(String[] args) {
+		int number = 23; // local variable......
+		System.out.println(number); // we can use local variable inside a particular method only...
+	}
+	 //System.out.println(number); it shows error bcoz local variable can't be used outside the method..
+}
